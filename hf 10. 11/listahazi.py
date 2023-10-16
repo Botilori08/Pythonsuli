76de74fa38a7ddf09be9c02ca4d14f8723ba209f
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2023-10-16 17:11:29 +0200

    házi

---
+++ hf 10. 11/listahazi.py
@@ -0,0 +1,16 @@
+lista=['sárga', 'piros','zöld','kék','lila']
+
+bekertszoveg="valami"
+while bekertszoveg!="":
+    bekeres=input("Írj be egy színt! ")
+    if bekertszoveg
+        
+bekert="alma"
+while bekert!="":
+    bekert=input("Írj be egy szamot! ")
+    if bekert!="":
+        lista.insert(bekert,"bekertszoveg")
+
+print(lista)
+        
+
