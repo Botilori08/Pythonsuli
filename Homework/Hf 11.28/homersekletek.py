4c6935c1b77fe23e942d1be1faf0f7e444ea9b85
Boti Lóránt Ferenc <botil22@ADY-NAGYATAD.SULINET.HU>
2023-11-28 10:47:37 +0100

    11.28 update

--- Homework/Hf 11.28/homersekletek.py
+++ Homework/Hf 11.28/homersekletek.py
@@ -1,11 +1,11 @@
 import random
 import math 
-def homersekletek(varosnev):
-    for i in range(24): 
-    i = random.randint(20,40)
-    varosnev.append(i)
-
-
+def homersekletek(lista):
+    for i in range(24):
+        random.randint(20,40)
+        lista.append(i)
+    print("A hőmérséklet ma óránként: ",lista)
+    
 Debrecen_hofok=[]
-
-print(homersekletek(Debrecen_hofok))
+lista=[]
+homersekletek(lista)
