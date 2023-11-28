feccc2780de583dcba182fdd058986756474c1b5
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2023-11-28 19:44:07 +0100

    Homework update

--- Homework/Hf 11.28/homersekletek.py
+++ Homework/Hf 11.28/homersekletek.py
@@ -1,10 +1,12 @@
+
+
 import random
 import math 
 def homersekletek(cname):
     for i in range(24):
         i = random.randint(20,37)
         lista.append(i)
-    print(cname + " hőmérséklet ma óránként: ",lista)
+    print(cname + " mai hőmérséklet adatok óránként: ",lista)
 
 
 
---
+++ Homework/Hf 11.28/homersekletek_menu.py
@@ -0,0 +1,78 @@
+import random
+import math 
+
+def homersekletek(cname):
+    for i in range(24):
+        i = random.randint(20,37)
+        lista.append(i)
+    print(cname + " mai hőmérséklet adatok óránként: ",lista)
+
+varosok=["Debrecen","Sopron","Budapest","Szeged","Kaposvár","Kilépés"]
+
+for i,elem in enumerate(varosok):
+    print("\t"+str(i+1)+":",elem,sep="")
+    
+varosId = "alma"
+
+while varosId == "alma" or varosId not in range(len(varosok)+1):
+    try:
+        varosId = int(input("Válaszd ki a kívánt várost! "))
+        if varosId not in range(len(varosok)+1):
+            raise
+    except:
+        print("Válassz a listából!")
+
+while varosId==1:
+    try:
+        lista=[]
+        Debrecen=[]
+        homersekletek("debreceni")
+        if varosId in range(len(varosok)+1):
+            break
+    except:
+        print("Válassz a listából!")
+
+        
+while varosId==2:
+    try:
+        lista=[]
+        Sopron=[]
+        homersekletek("soproni")
+        if varosId in range(len(varosok)+1):
+            break
+    except:
+        print("Válassz a listából!")
+
+while varosId==3:
+    try:
+        lista=[]
+        Budapest=[]
+        homersekletek("budapesti")
+        if varosId in range(len(varosok)+1):
+            break
+    except:
+        pass
+
+
+while varosId==4:
+    try:
+        lista=[]
+        Szeged=[]
+        homersekletek("szegedi")
+        if varosId in range(len(varosok)+1):
+            break
+    except:
+        pass
+
+while varosId==5:
+    try:
+        lista=[]
+        Kaposvár=[]
+        homersekletek("kaposvári")
+        if varosId in range(len(varosok)+1):
+            break 
+    except:
+        pass
+
+if varosId==6:
+    quit()
