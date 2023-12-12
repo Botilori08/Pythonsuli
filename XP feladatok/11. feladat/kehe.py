788c89f2af25d8501f1cb7803883be83ccfb06d9
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2023-12-12 15:48:07 +0100

    XP fealadat

---
+++ XP feladatok/11. feladat/kehe.py
@@ -0,0 +1,26 @@
+print("Gyártó: Boti Lóránt")
+print("2023.12.12."+"\n")
+abra=" "+"@"
+forditott="@"+" "
+
+
+print("@"+8*" "+"@")
+elsoIsmetles=" "+"@"+6*" "+"@"+" "+"\n"
+print(elsoIsmetles*3+elsoIsmetles.strip("\n"))
+print(" "+abra+"@"+2*" "+"@"+forditott+" "*2)
+masodikIsmetles=3*" "+abra+forditott+3*" "+"\n"
+print(masodikIsmetles*3+masodikIsmetles.strip("\n"))
+print(abra+"@"*6+forditott)
+
+
+# @        @
+#  @      @
+#  @      @
+#  @      @
+#  @      @
+#   @@  @@
+#     @@
+#     @@
+#     @@
+#     @@
+#  @@@@@@@@
---
+++ XP feladatok/szoroz.py
@@ -0,0 +1,5 @@
+szamlalo = (2.3*(4.5-3.7))*2
+nevezo = 8.0
+eredmeny= szamlalo/nevezo
+print(eredmeny)
+
