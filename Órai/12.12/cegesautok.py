ccaed2e4e6f56cd4b742bf3a52b2a05b4e43577a
Boti Lóránt Ferenc <botil22@ADY-NAGYATAD.SULINET.HU>
2023-12-12 09:41:08 +0100

    12.12

---
+++ Órai/12.12/cegesautok.py
@@ -0,0 +1,23 @@
+
+adatok = []
+
+f = open("autok.txt")
+#szöveg betöltése soronként
+for sor in f:
+    adatok.append(sor.strip().split(" ")) #így is jó: adatok.append(sor.split())
+f.close()
+
+for i in range(len(adatok)):   #lista bejárása indexekért 
+    adatok[i][0] = int(adatok[i][0]) #string átalakítás, megadott index = int
+    adatok[i][3] = int(adatok[i][3])
+    adatok[i][4] = int(adatok[i][4])
+    adatok[i][5] = int(adatok[i][5])  
+    temp= []
+    temp.append(adatok[i][1]) #időpontok indexe 
+    # '09:03' 
+    temp.append(int(adatok[i][1].split(":")[0]))
+    temp.append(int(adatok[i][1].split(":")[1]))
+    temp.append(temp[1]*60+temp[2]) #perccé alakítás 
+    
+    adatok[i][1]= temp #temp hozzáadása adatokhoz
+print(adatok)
