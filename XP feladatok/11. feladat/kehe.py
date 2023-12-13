4fd32101cc9351d0400803773cacee46c2b467c1
Boti Lóránt Ferenc <botil22@ADY-NAGYATAD.SULINET.HU>
2023-12-13 08:39:18 +0100

    12.13 first

--- XP feladatok/11. feladat/kehe.py
+++ XP feladatok/11. feladat/kehe.py
@@ -1,5 +1,6 @@
 print("Gyártó: Boti Lóránt")
 print("2023.12.12."+"\n")
+
 abra=" "+"@"
 forditott="@"+" "
 
@@ -7,7 +8,7 @@ forditott="@"+" "
 print("@"+8*" "+"@")
 elsoIsmetles=" "+"@"+6*" "+"@"+" "+"\n"
 print(elsoIsmetles*3+elsoIsmetles.strip("\n"))
-print(" "+abra+"@"+2*" "+"@"+forditott+" "*2)
+print(" "+abra+"@"+2*" "+"@"+forditott+" ")
 masodikIsmetles=3*" "+abra+forditott+3*" "+"\n"
 print(masodikIsmetles*3+masodikIsmetles.strip("\n"))
 print(abra+"@"*6+forditott)
--- XP feladatok/52. feladat/szoroz.py
+++ XP feladatok/52. feladat/szoroz.py
@@ -2,4 +2,3 @@ szamlalo = (2.3*(4.5-3.7))*2
 nevezo = 8.0
 eredmeny= szamlalo/nevezo
 print(eredmeny)
-
