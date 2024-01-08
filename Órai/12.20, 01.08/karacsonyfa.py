dee04f813cf64318c747772c29c23c271077c875
Boti Lóránt Ferenc <botil22@ADY-NAGYATAD.SULINET.HU>
2024-01-08 14:06:22 +0100

    01.08

--- Órai/12.20, 01.08/karacsonyfa.py
+++ Órai/12.20, 01.08/karacsonyfa.py
@@ -18,7 +18,7 @@ win.geometry("600x600")
 #Create a canvas widget 
 canvas = Canvas(win, width=600, height=600)
 canvas.configure(bg="lightgray")
-canvas.pack(fill=BOTH, expand=1)
+canvas.pack(fill=BOTH, expand=1) #teljes ablakot kitölti 
 
 
 pontok=[0,0 ,100, 0 ,100 ,100 , 0, 100, 0,0]
@@ -34,6 +34,18 @@ for i in range(0,len(pontok),2):
 canvas.create_line(pontok,width=5, fill="green")
 pontok=eltol(pontok,100,0)
 canvas.create_line(pontok,width=5, fill="green")
+
+fenyo1=[200,0,0,400,190,400,190,500,210,500,210,400,400,400,200,0]
+pontok=eltol(fenyo1,10,10)
+canvas.create_line(fenyo1,width=5, fill="green")
+
+
+
+
+
+
+
+
 
 
 win.mainloop()
