fc8839b6da8aa516d0c97971bde7b5359aa1763e
Boti Lóránt Ferenc <botil22@ADY-NAGYATAD.SULINET.HU>
2024-01-17 08:46:42 +0100

    01.17

---
+++ XP feladatok/370. feladat/kockabelgomb.py
@@ -0,0 +1,4 @@
+a = int(input("Mennyi legyen az a oldal? "))
+r = int(input("Mennyi legyen a beírt kör sugara? "))
+R = a/2
+print("A beírt kör sugarának hossza")
---
+++ XP feladatok/801. feladat/try.py
@@ -0,0 +1,6 @@
+szamok = []
+while szamok == [] or bekert > 0 : 
+    bekert= int(input("Írj be számokat! "))
+    szamok.append(bekert)
+
+print(szamok)
