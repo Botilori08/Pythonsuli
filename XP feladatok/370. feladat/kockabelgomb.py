3dd0f50775137a30b4561df0f28d250db59f21ac
Boti Lóránt Ferenc <botil22@ADY-NAGYATAD.SULINET.HU>
2024-01-19 08:37:50 +0100

    01.19

--- XP feladatok/370. feladat/kockabelgomb.py
+++ XP feladatok/370. feladat/kockabelgomb.py
@@ -1,4 +1,14 @@
-a = int(input("Mennyi legyen az a oldal? "))
-r = int(input("Mennyi legyen a beírt kör sugara? "))
-R = a/2
-print("A beírt kör sugarának hossza"+ R)
+print("Ez a program kiszámolja a kocka oldalhosszából a kockába írható gömb sugarát.")
+a = int(input("Mennyi legyen az első kocka oldala? "))
+
+R= a/2
+text = "Az első kockába beírható kör sugara: {R}"
+
+print(text.format(R= a/2))
+
+a = int(input("Mennyi legyen a második kocka oldala? "))
+
+R= a/2
+text = "A második kocklába beírható kör sugara: {R}"
+
+print(text.format(R= a/2))
