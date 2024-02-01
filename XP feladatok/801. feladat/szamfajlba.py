432b4fc48ba23b232466c45140ed82e1d6d35aa8
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2024-02-01 16:21:15 +0100

    02.01

--- XP feladatok/801. feladat/szamfajlba.py
+++ XP feladatok/801. feladat/szamfajlba.py
@@ -1,6 +1,6 @@
 szamok = []
 while szamok == [] or bekert > 0 : 
-    bekert= int(input("Írj be számokat! "))
+    bekert= int(input("Írj be egy számot! "))
     szamok.append(bekert)
     if bekert ==0:
         break
