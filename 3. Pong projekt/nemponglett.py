c19c8b92f8f4c67a49f6d0a910dbad136e61ba71
Boti Lóránt Ferenc <botil22@ADY-NAGYATAD.SULINET.HU>
2024-02-05 13:59:16 +0100

    wexen

--- 3. Pong projekt/nemponglett.py
+++ 3. Pong projekt/nemponglett.py
@@ -12,10 +12,7 @@ def randomcolor():
     g = random.randint(0,255)
     b = random.randint(0,255)
     return ("#"+hex(r)[-2:]+hex(g)[-2:]+hex(b)[-2:]).replace("x","0")
-
 jatekHatter = "lightgray"
-jatekSpeed = 100
-
 def atmenetColor(red,green,blue):
     red+=5
     if red>255:
@@ -28,25 +25,6 @@ def atmenetColor(red,green,blue):
         blue = blue-255
 
     return ("#"+hex(red)[-2:]+hex(green)[-2:]+hex(blue)[-2:]).replace("x","0"),red, green, blue
-
-def rajzol():
-        labdaPos[0]+= labdaSpeed[0]
-        labdaPos[1]+= labdaSpeed[1]
-
-        if labdaPos[0] > win.winfo_width() or labdaPos[0]<0:
-            labdaSpeed[0]*=-1
-            #labdaColor = randomcolor()
-        if labdaPos[1] > win.winfo_height() or labdaPos[1]<0:
-            labdaSpeed[1]*=-1
-            #labdaColor = randomcolor()
-
-        labdaLista.append(canvas.create_oval(labdaPos[0],labdaPos[1],labdaPos[0]+labdaSize,labdaPos[1]+labdaSize, fill= labdaColor, outline = ""))
-
-        if len(labdaLista) > labdaListaHossz:
-            canvas.delete(labdaLista[0])
-            labdaLista.pop(0)
-            
-        win.after(jatekSpeed,rajzol)
     
 print(randomcolor())
 
@@ -61,22 +39,36 @@ canvas.pack(fill=BOTH, expand= 1)
 
 labda = canvas.create_oval(0,0,100,100,fill="red")
 
-labdaSpeed = [5,5.25]
+labdaSpeed = [1,30]
 labdaPos = [200,200]
 labdaSize = 50
 labdaColor = "red"
 red, green, blue = 0,0,0
 labdaLista = []
-labdaListaHossz= 1
+labdaListaHossz= 100
 
-win.after(jatekSpeed,rajzol)
-win.mainloop()
 
 while True:
-    #labdaColor, red, green, blue=atmenetColor(red,green,blue)
-    #print(labdaColor)
-    rajzol()
+    labdaColor, red, green, blue=atmenetColor(red,green,blue)
+    print(labdaColor)
+    labdaPos[0]+= labdaSpeed[0]
+    labdaPos[1]+= labdaSpeed[1]
+
+    if labdaPos[0] > win.winfo_width() or labdaPos[0]<0:
+        labdaSpeed[0]*=-1
+        labdaColor = randomcolor()
+    if labdaPos[1] > win.winfo_height() or labdaPos[1]<0:
+        labdaSpeed[1]*=-1
+        labdaColor = randomcolor()
+
+    labdaLista.append(canvas.create_oval(labdaPos[0],labdaPos[1],labdaPos[0]+labdaSize,labdaPos[1]+labdaSize, fill= labdaColor, outline = ""))
+
+    if len(labdaLista) > labdaListaHossz:
+        canvas.delete(labdaLista[0])
+        labdaLista.pop(0)
+    
     canvas.update()
 
 
 win.mainloop()
+
