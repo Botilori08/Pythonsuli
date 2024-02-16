c899f9e4ff4a926363d424ea215040cd3e8aeebe
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-02-16 13:21:15 +0100

    Áramkör projekt

---
+++ 4. projekt - Áramkörök/aramkor.py
@@ -0,0 +1,49 @@
+#Áramkör projekt
+#2024.02.16.
+
+class elem:
+    x = 0
+    y = 0
+
+    meret = 100
+    def __init__ (self,x,y,meret,canvas):
+        self.x = x
+        self.y = y
+        self.meret = meret
+        self.canvas = canvas
+        self.rajz()
+
+    def rajz(self):
+        print(canvas)
+        canvas.create_rectangle(self.x,self.y,
+                              self.x+self.meret,self.y+self.meret,
+                              fill="gray")
+        vonalak = [
+            [
+                self.x, self.y+self.meret*0.5,
+                self.x + self.meret*0.45, self.y+ self.meret*0.5
+            ],
+        ]
+        for egyvonal in vonalak:
+            self.canvas.create_line(egyvonal)
+
+from tkinter import *
+
+win = Tk()
+win.geometry("600x620+100+20")
+win.title("Áramkör")
+canvas = Canvas(win, bg="white")
+print(canvas)
+
+
+canvas.pack(fill=BOTH, expand= 1)
+
+
+elem1 = elem(0,0,100,canvas)
+#elem1.rajz()
+
+elem2 = elem(200,100,50,canvas)
+#elem2.rajz()
+
+
+win.mainloop()
