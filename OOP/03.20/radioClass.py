aac0ee7f0d720289047caf85f1aaf584c99ed787
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-03-20 08:46:16 +0100

    03.20

---
+++ OOP/03.20/radioClass.py
@@ -0,0 +1,7 @@
+class uzenet:
+    def __init__(self,sor1,sor2):
+        #1 13
+        #abor# #e#tun###agy#szel#2# #o##h#d#g ##rkasn#o#oka# #a#tunk e####a#akn##$#$#$##$$$$$$####
+        self.nap = int(sor1.split(" ")[0])
+        self.radiosszam = int(sor1.split(" ")[1])
+
