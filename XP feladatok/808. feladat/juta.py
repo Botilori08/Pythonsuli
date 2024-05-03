a2ad0748dc2478b7401742301b8c5cb00f58622d
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-05-03 10:10:13 +0200

    05.03

---
+++ XP feladatok/808. feladat/juta.py
@@ -0,0 +1,39 @@
+import math
+class Dolgozo:
+    def __init__(self,sor):
+        vag = sor.split(";")
+        self.azonosito = int(vag[0])
+        self.nev = vag[1]
+        self.anyjaneve = vag[2]
+        self.telepules = vag[3]
+        self.cim = vag[4]
+        self.fizetes = int(vag[5])
+        self.jutalom = int(vag[6])
+        self.belepes = vag[7]
+        self.szuletes = vag[8]
+        self.szulhely = vag[9]
+
+f = open("dolgozok.txt",encoding="utf8" )
+lista= []
+for egySor in f:
+    lista.append(Dolgozo(egySor))
+f.close()
+
+jutalmak = 0
+for elem in lista:
+    jutalmak += elem.jutalom
+print("A jutalmak összege {} Ft".format(jutalmak))
+
+fizetesek = []
+osszFizetes = 0
+for egyFizetes in lista:
+    fizetesek.append(egyFizetes.fizetes)
+for egyErtek in fizetesek:
+    osszFizetes += egyErtek
+print("A fizetések átlaga: {} Ft".format(math.floor(osszFizetes / len(fizetesek))))
+
+for egylakhely in lista:
+    if egylakhely.telepules == "Recsk":
+        print("Van recski dolgozó.")
+    else:
+        print("Nincs recski dolgozó.")
