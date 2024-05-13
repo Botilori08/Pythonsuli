a93b2d42d6d5c6cdf6f3c3e7075368e3f8517b36
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2024-05-13 17:29:40 +0200

    05.13

--- Órai/05.13/F2modul.py
+++ Órai/05.13/F2modul.py
@@ -1,32 +1,42 @@
-def haromszog():
-    lista = []
+def haromszog(lista):
     while lista == []:
         try:
-            szam1 = int(input("Kérem a(z) 1. egész szamot! "))
-            lista.append(szam1)
+            elsoSzam = int(input("Kérem a(z) 1. egész szamot! "))
+            lista.append(elsoSzam)
         except:
             print("Ez nem egész szám!")
     while len(lista) < 2:
         try:
-            szam2 = int(input("Kérem a(z) 2. egész szamot! "))
-            lista.append(szam2)
+            masodikSzam = int(input("Kérem a(z) 2. egész szamot! "))
+            lista.append(masodikSzam)
         except:
             print("Ez nem egész szám!")
-
     while len(lista) < 3:
         try:
-            szam3 = int(input("Kérem a(z) 3. egész szamot! "))
-            lista.append(szam2)
+            harmadikSzam = int(input("Kérem a(z) 3. egész szamot! "))
+            lista.append(harmadikSzam)
         except:
             print("Ez nem egész szám!")
 
-    return print(lista)
+    return lista
 
 def haromHaromszog():
     adatsor1 = []
     adatsor2 = []
     adatsor3 = []
-    if adatsor1 == []:
-        haromszog()
-        
-    return adatsor1
+    print("1.adatsor")
+    while adatsor1 == []:
+        haromszog(adatsor1)
+        if len(adatsor1) < 4:
+            break
+    print("2.adatsor")
+    while adatsor2 == []:
+        haromszog(adatsor2)
+        if len(adatsor2) < 4:
+            break
+    print("3.adatsor")
+    while adatsor3 == []:
+        haromszog(adatsor3)
+        if len(adatsor3) < 4:
+            break
+    return print(adatsor1), print(adatsor2),print(adatsor3)
