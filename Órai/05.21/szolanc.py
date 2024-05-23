43a3981361ae9abf8a64f689da8f1fe749272daf
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2024-05-23 21:13:05 +0200

    05.23

--- Órai/05.21/szolanc.py
+++ Órai/05.21/szolanc.py
@@ -1,15 +1,14 @@
 szavak = []
 sorszam = 1
-elozo = ""
 lefutasok = 0
-while len(szavak) < 20:
+while True:
      szo = input("Kérem a(z) {}. szót! ".format(sorszam))
      sorszam += 1
-     szavak.append(szo)
-     if len(szo) > 6:
+     
+     if len(szo) != 6:
           print("A karakterek száma téves!")
           break
-     if lefutasok != 0:
+     elif lefutasok != 0:
           if szavak[-1][-1] == szo[0]:
                szavak.append(szo)
           else:
@@ -17,4 +16,13 @@ while len(szavak) < 20:
                break
      else:
           lefutasok += 1
-    
+          szavak.append(szo)
+
+lepesSzam = len(szavak)
+print("Helyes lépések száma: {}".format(lepesSzam))
+if lepesSzam in [0,1,2]:
+     print("Szint: kezdő")
+elif lepesSzam in [3,4,5]:
+     print("Szint: közepes")
+else:
+     print("Szint: haladó")
