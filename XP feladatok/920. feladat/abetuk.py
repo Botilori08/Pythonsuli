8e17d7195a8f7bc40c54dd621d0f4f66266a6ffb
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-05-23 12:42:42 +0200

    05.23

--- XP feladatok/920. feladat/abetuk.py
+++ XP feladatok/920. feladat/abetuk.py
@@ -2,10 +2,11 @@ def aSzamolo(szoveg):
     adarab = 0
     betuLista = []
     for e in szoveg:
-            if e == 'a':
-                adarab += 1
-                return print("A megadott szóban {} darab 'a' betű van".format(adarab))
-            else:
-                return print("A megadott szóban nincs 'a' betű")
+        if e == 'a':
+            adarab += 1
+    if adarab == 0:
+        print("A megadott szóban nincs 'a' betű")
+    else:
+        print("A megadott szóban {} db 'a' betű van".format(adarab))
 
 aSzamolo("alap")
