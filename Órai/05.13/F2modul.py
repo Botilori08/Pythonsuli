b181a71d0e65349786d93d57fbb9c1563600dc06
Botilori08 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2024-05-26 10:12:05 +0200

    05.26

--- Órai/05.13/F2modul.py
+++ Órai/05.13/F2modul.py
@@ -1,42 +1,40 @@
 def haromszog(lista):
-    while lista == []:
+    lista = []
+    while len(lista)<3:
         try:
-            elsoSzam = int(input("Kérem a(z) 1. egész szamot! "))
-            lista.append(elsoSzam)
-        except:
-            print("Ez nem egész szám!")
-    while len(lista) < 2:
-        try:
-            masodikSzam = int(input("Kérem a(z) 2. egész szamot! "))
-            lista.append(masodikSzam)
-        except:
-            print("Ez nem egész szám!")
-    while len(lista) < 3:
-        try:
-            harmadikSzam = int(input("Kérem a(z) 3. egész szamot! "))
-            lista.append(harmadikSzam)
+            bekert = int(input("Kérem a(z) {}. egész szamot! ".format(len(lista)+1)))
+            lista.append(bekert)
+            if len(lista) == 3:
+                break
         except:
             print("Ez nem egész szám!")
 
     return lista
 
 def haromHaromszog():
+    adatsor = []
     adatsor1 = []
     adatsor2 = []
     adatsor3 = []
-    print("1.adatsor")
-    while adatsor1 == []:
-        haromszog(adatsor1)
-        if len(adatsor1) < 4:
+    adatsorSzama = 0
+    while len(adatsor) < 3:
+        print("{}.adatsor".format(adatsorSzama +1))
+        adatsor.append(haromszog(adatsor1))
+        print("{}.adatsor".format(adatsorSzama +2))
+        adatsor.append(haromszog(adatsor2))
+        print("{}.adatsor".format(adatsorSzama +3))
+        adatsor.append(haromszog(adatsor3))
+        if len(adatsor) == 3 :
             break
-    print("2.adatsor")
-    while adatsor2 == []:
-        haromszog(adatsor2)
-        if len(adatsor2) < 4:
-            break
-    print("3.adatsor")
-    while adatsor3 == []:
-        haromszog(adatsor3)
-        if len(adatsor3) < 4:
-            break
-    
+    print("\t a={}, b={}, c={}".format(adatsor[0][0],adatsor[0][1],adatsor[0][2]))
+    print("\t a={}, b={}, c={}".format(adatsor[1][0],adatsor[1][1],adatsor[1][2]))
+    print("\t a={}, b={}, c={}".format(adatsor[2][0],adatsor[2][1],adatsor[2][2]))
+
+    return adatsor
+
+def oldalvizsgalo(lista):
+    lista.sort()
+    for i in range(len(lista)):
+        if lista[i][0] + lista[i][1] > lista[i][-1] and lista[i][0] + lista[i][1] > lista[i][-1]:
+        
+
