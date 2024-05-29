37af9781360f6fc9ac4dffeafc56fead9e0845f4
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-05-29 08:46:40 +0200

    05.29

---
+++ XP feladatok/79. feladat/aszfalt.py
@@ -0,0 +1,6 @@
+#V = a*b*c
+# a = szélesség = 22m = 
+# b = vastagság = 12cm =
+# c = hossz = 3,5km = 3500 m
+
+a =  
---
+++ Órai/05.29/05.29 feladat.py
@@ -0,0 +1,30 @@
+
+szam = 1
+szamLista = []
+
+while szam != 0:
+    szam = int(input("Kérek egy számot: "))
+    if szam != 0:
+        szamLista.append(szam)
+#print(szamLista)
+
+pozitiv = 0
+negativ = 0
+for egySzam in szamLista:
+    if egySzam < 0:
+        negativ += egySzam
+    else:
+        pozitiv += egySzam
+print("A pozitív számok összege: {}".format(pozitiv))
+print("A negatív számok összege: {}".format(negativ))
+
+if abs(negativ) > abs(pozitiv):
+    print("A negatív számok összege távolabb van nullától!")
+elif abs(pozitiv) > abs(negativ) :
+    print("A pozitív számok összege távolabb van nullától!")
+else:
+    print("A pozitív és negatív számok nullától számított távolsága egyenlő!")
+
+listaFele = szamLista[:len(szamLista)//2]
+
+print(listaFele)
