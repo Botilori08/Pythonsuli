ac88f431606b72e38608e85be82a832e52dab149
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-05-31 10:38:55 +0200

    05.31

---
+++ Órai/05.31/05.31.py
@@ -0,0 +1,22 @@
+def bekero():
+    global szam
+    szam = 0
+    while szam == 0:
+        try:
+            szam = int(input("Kérek egy számot!: "))
+        except:
+            print("Ez nem egész szám!")
+    return szam
+
+szamok = []
+Ujszam = bekero()
+print(Ujszam)
+while Ujszam >= 0:
+    szamok.append(Ujszam)
+    if szam < 0:
+        break
+
+
+    
+
+print(szamok)
